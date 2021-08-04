89cba59f5e2c2b1e6259682c0c2c544e1799ffc7
lgu3877 <lgu3877@naver.com>
2021-08-04 16:58:15 +0900

    chore : mediaconvert Rotate 90 20210804 16:58

--- common/utils/mediaConvertUtil.js
+++ common/utils/mediaConvertUtil.js
@@ -164,8 +164,151 @@ module.exports =  function (final_name, video_width, video_height) {
                                 "DefaultSelection": "DEFAULT"
                             }
                         },
+                        "VideoSelector": {},
+                        "TimecodeSource": "ZEROBASED",
+                        "ImageInserter": {
+                            "InsertableImages": [
+                                {
+                                    "Width": parseInt(video_width / 6.3),
+                                    "Height": parseInt(video_height / 14.8),
+                                    "ImageX": parseInt(video_width / 1.25),
+                                    "ImageY": parseInt(video_height / 2.4),
+                                    "Layer": 1,
+                                    "ImageInserterInput": `${funcUtil.getAWSMediaConvertS3StartingPoint()}wegglelogo.png`,
+                                    "Opacity": 50
+                                }
+                            ]
+                        },
+                        "FileInput": "null"
+                        // "FileInput": "s3://test341/04ef74af4025fdfe30ff93beda25ee65.mp4"
+                    }
+                ]
+            },
+            "AccelerationSettings": {
+                "Mode": "DISABLED"
+            },
+            "StatusUpdateInterval": "SECONDS_60",
+            "Priority": 0,
+            "HopDestinations": []
+        }
+
+
+
+        let paramsRotate = {
+            "Queue": funcUtil.getAWSMediaConvertQueue(),
+            "UserMetadata": {
+                "Customer": "Amazon"
+            },
+            "Role": funcUtil.getAWSMediaConvertRole(),
+            "Settings": {
+                "TimecodeConfig": {
+                    "Source": "ZEROBASED"
+                },
+                "OutputGroups": [
+                    {
+                        "CustomName": "grege",
+                        "Name": "File Group",
+                        "Outputs": [
+                            {
+                                "ContainerSettings": {
+                                    "Container": "MP4",
+                                    "Mp4Settings": {
+                                        "CslgAtom": "EXCLUDE",
+                                        "FreeSpaceBox": "EXCLUDE",
+                                        "MoovPlacement": "NORMAL"
+                                    }
+                                },
+                                "VideoDescription": {
+                                    "Width": 574,
+                                    "ScalingBehavior": "DEFAULT",
+                                    "Height": 1024,
+                                    "VideoPreprocessors": {
+                                        "Deinterlacer": {
+                                            "Algorithm": "INTERPOLATE",
+                                            "Mode": "DEINTERLACE",
+                                            "Control": "NORMAL"
+                                        }
+                                    },
+                                    "TimecodeInsertion": "DISABLED",
+                                    "AntiAlias": "ENABLED",
+                                    "Sharpness": 50,
+                                    "CodecSettings": {
+                                        "Codec": "H_264",
+                                        "H264Settings": {
+                                            "ParNumerator": 16,
+                                            "ParDenominator": 9,
+                                            "Bitrate": 1500000
+                                        }
+                                    },
+                                    "AfdSignaling": "NONE",
+                                    "DropFrameTimecode": "ENABLED",
+                                    "RespondToAfd": "NONE",
+                                    "ColorMetadata": "INSERT"
+                                },
+                                "AudioDescriptions": [
+                                    {
+                                        "AudioTypeControl": "FOLLOW_INPUT",
+                                        "CodecSettings": {
+                                            "Codec": "AAC",
+                                            "AacSettings": {
+                                                "AudioDescriptionBroadcasterMix": "NORMAL",
+                                                "Bitrate": 160000,
+                                                "RateControlMode": "CBR",
+                                                "CodecProfile": "LC",
+                                                "CodingMode": "CODING_MODE_2_0",
+                                                "RawFormat": "NONE",
+                                                "SampleRate": 48000,
+                                                "Specification": "MPEG4"
+                                            }
+                                        },
+                                        "LanguageCodeControl": "FOLLOW_INPUT",
+                                        "AudioType": 0
+                                    }
+                                ],
+                                // "NameModifier": "test1111"
+                                "NameModifier": "ConvertSuccess"
+                            },
+                            {
+                                "ContainerSettings": {
+                                    "Container": "RAW"
+                                },
+                                "VideoDescription": {
+                                    "Width": 1080,
+                                    "Height": 1920,
+                                    "CodecSettings": {
+                                        "Codec": "FRAME_CAPTURE",
+                                        "FrameCaptureSettings": {
+                                            "FramerateNumerator": 30,
+                                            "FramerateDenominator": 88,
+                                            "MaxCaptures": 1,
+                                            "Quality": 80
+                                        }
+                                    }
+                                },
+                                "Extension": "jpg",
+                                "NameModifier": "Thumbnail"
+                            }
+                        ],
+                        "OutputGroupSettings": {
+                            "Type": "FILE_GROUP_SETTINGS",
+                            "FileGroupSettings": {
+                                // "Destination": "s3://test341/"
+                                // "Destination": "s3://weggle-bucket/"
+                                "Destination": funcUtil.getAWSMediaConvertS3Destination()
+                            }
+                        }
+                    }
+                ],
+                "Inputs": [
+                    {
+                        "AudioSelectors": {
+                            "Audio Selector 1": {
+                                "DefaultSelection": "DEFAULT"
+                            }
+                        },
+
                         "VideoSelector": {
-                           "Rotate": (video_width > video_height) ?  "DEGREES_90" : ""
+                           "Rotate": "DEGREES_90"
                         },
                         "TimecodeSource": "ZEROBASED",
                         "ImageInserter": {
@@ -198,7 +341,7 @@ module.exports =  function (final_name, video_width, video_height) {
 
         console.log('final_name' + final_name)
 
-        const data = convertFunc(final_name, params)
+        const data = convertFunc(final_name, video_width < video_height ? params : paramsRotate)
 
         if(data) {
             let basename = path.basename(final_name, extname);
