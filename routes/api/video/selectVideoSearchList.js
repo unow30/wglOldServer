f4a0b88ca79abb8dc9330a6c8cd076ef961e86c6
lgu3877 <lgu3877@naver.com>
2021-08-19 14:24:51 +0900

    feat : video Search api
    2021-08-19 14:24

---
+++ routes/api/video/selectVideoSearchList.js
@@ -0,0 +1,138 @@
+/**
+ * Created by gunucklee on 2021. 08. 19.
+ *
+ * @swagger
+ * /api/private/video/search/list:
+ *   get:
+ *     summary: 영상 검색 정보
+ *     tags: [Video]
+ *     description: |
+ *       path : /api/private/video/search/list
+ *
+ *       * 영상 검색 정보
+ *
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         required: true
+ *         default: 65535
+ *         schema:
+ *           type: number
+ *           example: 65535
+ *         description: |
+ *           카테고리 (비트 연산)
+ *           ==> 65535 : 모든 상품
+ *           ==> 멀티선택의 경우 코드 값을 합치면됨
+ *           ==> ex) 1+8+32 = 41
+ *           * 1 : 식품
+ *           * 2 : 뷰티
+ *           * 4 : 홈데코
+ *           * 8 : 패션잡화
+ *           * 16 : 반려동물
+ *           * 32 : 유아
+ *           * 64 : 스포츠레저
+ *           * 128 : 식물
+ *       - in: query
+ *         name: random_seed
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: 133q1234
+ *         description: |
+ *           검색할 때 필요한 랜덤 시드입니다.
+ *       - in: query
+ *         name: offset
+ *         default: 0
+ *         required: true
+ *         schema:
+ *           type: number
+ *           example: 0
+ *         description: |
+ *           페이지 시작 값을 넣어주시면 됩니다. Limit 30
+ *           offset 0: 0~30
+ *           offset 30: 30~60
+ *           offset 60: 60~90
+ *       - in: query
+ *         name: keyword
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example:
+ *         description: 검색 키워드(상품명 검색)
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *         schema:
+ *           $ref: '#/definitions/Video'
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+        // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = await querySelect(req, db_connection);
+
+            deleteBody(req)
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+    // paramUtil.checkParam_noReturn(req.paramBody, 'video_uid');
+    // paramUtil.checkParam_noReturn(req.paramBody, 'social_id');
+}
+
+function deleteBody(req) {
+    // delete req.innerBody['item']['longitude']
+    // delete req.innerBody['item']['push_token']
+    // delete req.innerBody['item']['access_token']
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_select_video_search_list'
+        , [
+            req.headers['user_uid'],
+            req.paramBody['category'],
+            req.paramBody['keyword'],
+            req.paramBody['random_seed'],
+            req.paramBody['offset'],
+        ]
+    );
+}
+
+
