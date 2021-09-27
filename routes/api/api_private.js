96b7a691adeec0c7a3a4ef902d37364198416569
unow30 <https://github.com/unow30>
2021-09-27 17:43:50 +0900

    fix: updateOrderConfirm.js 제거
    사용하지 않는 컨트롤러 제거
    (파일 백업함)
    
    2021-07-27 17:43

--- routes/api/api_private.js
+++ routes/api/api_private.js
@@ -46,7 +46,7 @@ app.route('/cart/list').get( require('./cart/selectCartList') )
 app.route('/order').post( require('./order/createOrder') )
 app.route('/order/list').get( require('./order/selectOrderList') )
 app.route('/order/detail').get( require('./order/selectOrderDetail') )
-app.route('/order/confirm').put( require('./order/updateOrderConfirm') )
+// app.route('/order/confirm').put( require('./order/updateOrderConfirm') )
 app.route('/order/status').put(require('../middleware/bootPay'), require('./order/updateOrderStatus') )
 
 /**
