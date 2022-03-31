fc1be0d179475e18b4900893498888506e3583b7
weggleorange <weggle_orange@weggle.io>
2022-03-31 13:03:12 +0900

    chore: m3u8 update 2022-03-31 10:34

---
+++ routes/api/feed/selectFeedList_m3u8.js
@@ -0,0 +1,91 @@
+/**
+ * Created by hyunhunhwang on 2021. 01. 07.
+ *
+ * @swagger
+ * /api/private/feed/list/m3u8:
+ *   get:
+ *     summary: 피드 목록
+ *     tags: [Feed]
+ *     description: |
+ *       path : /api/private/feed/list/m3u8
+ *
+ *     parameters:
+ *       - in: query
+ *         name: offset
+ *         default: 0
+ *         required: true
+ *         schema:
+ *           type: number
+ *           example: 0
+ *         description: |
+ *           페이지 시작 값을 넣어주시면 됩니다. Limit 30
+ *           offset 0: 0~30
+ *           offset 30: 30~60
+ *           offset 60: 60~90
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *         schema:
+ *           $ref: '#/definitions/FeedListApi'
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+        logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = await querySelect(req, db_connection);
+
+            deleteBody(req)
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+}
+
+function deleteBody(req) {
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_select_feed_list_m3u8'
+        , [
+            req.headers['user_uid'],
+            req.paramBody['offset']
+        ]
+    );
+}
