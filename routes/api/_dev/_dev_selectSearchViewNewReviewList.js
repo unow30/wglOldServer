bcb6f1c5323a079b74397c10747cf25c8845f186
jonghao2 <weggle_khaki@weggle.io>
2022-05-25 13:09:20 +0900

    count 로직 수정

--- routes/api/_dev/_dev_selectSearchViewNewReviewList.js
+++ routes/api/_dev/_dev_selectSearchViewNewReviewList.js
@@ -29,80 +29,86 @@
  *           $ref: '#/definitions/Error'
  */
 
- const paramUtil = require('../../../common/utils/paramUtil');
- const fileUtil = require('../../../common/utils/fileUtil');
- const mysqlUtil = require('../../../common/utils/mysqlUtil');
- const sendUtil = require('../../../common/utils/sendUtil');
- const errUtil = require('../../../common/utils/errUtil');
- const logUtil = require('../../../common/utils/logUtil');
- 
- let file_name = fileUtil.name(__filename);
- 
- module.exports = function (req, res) {
-     const _funcName = arguments.callee.name;
- 
-     try {
-         console.log(12312312312344444)
-         req.file_name = file_name;
-         logUtil.printUrlLog(req, `== function start ==================================`);
-         req.paramBody = paramUtil.parse(req);
-         // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
- 
-         checkParam(req);
-         
-         mysqlUtil.connectPool(async function (db_connection) {
-             req.innerBody = {};
- 
-             let count_data = await querySelectCount(req, db_connection);
-             req.innerBody['item'] = await querySelect(req, db_connection);
-             req.innerBody['total_count'] = count_data['total_count'];
-             let date = new Date(Date.now())
-             date = date.setMonth(date.getMonth()-6)
-             if(date > req.innerBody.item[0].created_time || !req.innerBody.item[0].created_time){
-                 
-                 const err = new Error('더이상 최신 데이터가 없습니다.')
-                 err.code = 400
- 
-                 return sendUtil.sendErrorPacket(req, res, err);
-             }
-             deleteBody(req);
-             sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
- 
-         }, function (err) {
-             sendUtil.sendErrorPacket(req, res, err);
-         });
- 
-     } catch (e) {
-         let _err = errUtil.get(e);
-         sendUtil.sendErrorPacket(req, res, _err);
-     }
- }
- 
- function checkParam(req) {
- }
- 
- function deleteBody(req) {
- }
- 
- function querySelectCount(req, db_connection) {
-     const _funcName = arguments.callee.name;
- 
-     return mysqlUtil.querySingle(db_connection
-         , 'call proc_select_searchview_new_review_list_count'
-         , [
-             req.headers['user_uid']
-         ]
-     );
- }
- 
- function querySelect(req, db_connection) {
-     const _funcName = arguments.callee.name;
- 
-     return mysqlUtil.queryArray(db_connection
-         , 'call proc_select_searchview_new_review_list2'
-         , [
-             req.headers['user_uid']
-           , req.paramBody['offset']??0
-         ]
-     );
- }
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        console.log(12312312312344444)
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+        // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            let date = new Date()
+            date = new Date(date.setMonth(date.getMonth()-6))
+            req.headers.date = date
+            
+            let count_data = await querySelectCount(req, db_connection);
+            req.innerBody['item'] = await querySelect(req, db_connection);
+            req.innerBody['total_count'] = count_data['total_count'];
+
+
+
+            if(date > req.innerBody.item[0].created_time || !req.innerBody.item[0].created_time){
+
+                const err = new Error('더이상 최신 데이터가 없습니다.')
+                err.code = 400
+
+                return sendUtil.sendErrorPacket(req, res, err);
+            }
+            deleteBody(req);
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+}
+
+function deleteBody(req) {
+}
+
+function querySelectCount(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_searchview_new_review_list_count'
+        , [
+            req.headers['user_uid'],
+            req.headers['date']
+        ]
+    );
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_select_searchview_new_review_list2'
+        , [
+            req.headers['user_uid']
+            , req.paramBody['offset']??0
+        ]
+    );
+}
