80ada18ab8c0f3b446fe6294716ef3b5da8ac4e0
jonghao2 <weggle_khaki@weggle.io>
2022-06-27 10:25:29 +0900

    MODIFY: home 쿼리 콜 병렬처리

--- routes/api/searchView/selectSerchViewListAll.js
+++ routes/api/searchView/selectSerchViewListAll.js
@@ -59,8 +59,17 @@ module.exports = function (req, res) {
             req.innerBody = {};
 
             req.innerBody['ad_list'] = queryADList(req, db_connection);
+            console.log('=====================================>>>>>>>>>')
+            console.log(req.innerBody['ad_list'])
+            console.log('=====================================>>>>>>>>>')
             req.innerBody['new_product_preview_list'] = queryNewProductPreviewList(req, db_connection);
+            console.log('=====================================>>>>>>>>>')
+            console.log(req.innerBody['new_product_preview_list'])
+            console.log('=====================================>>>>>>>>>')
             req.innerBody['new_review_preview_list'] = queryNewReviewPreviewList(req, db_connection);
+            console.log('=====================================>>>>>>>>>')
+            console.log(req.innerBody['new_review_preview_list'])
+            console.log('=====================================>>>>>>>>>')
 
             //위글딜 프리뷰 한 프로시저로 도전
             req.innerBody['weggle_deal_preview_list'] = await queryWeggledealSeller(req, db_connection);
@@ -80,6 +89,9 @@ module.exports = function (req, res) {
 
             // req.innerBody['category_product_preview_list'] = await queryCategoryProductPreviewList(req, db_connection);
             req.innerBody['best_review_list'] = queryBestReviewList(req, db_connection);
+            console.log('=====================================>>>>>>>>>')
+            console.log(req.innerBody['best_review_list'])
+            console.log('=====================================>>>>>>>>>')
 
             await Promise.all(
                 req.innerBody['ad_list'],
