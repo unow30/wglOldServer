6d12c2c0ee029e9d1bb417f0a12836ea2e8ff387
jonghao2 <weggle_khaki@weggle.io>
2022-08-08 14:34:46 +0900

    ADD: 홈뷰 기획전 프로시저 추가

--- routes/api/searchView/v1SelectSerchViewListAll.js
+++ routes/api/searchView/v1SelectSerchViewListAll.js
@@ -53,16 +53,19 @@
             const gongu_deal = queryGonguDeal(req, db_connection); // 지금뜨는 공구딜
             const gongu_deadline = queryGonguDeadline(req, db_connection); // 시간이 얼마 안남은 공구
             const hot_weggler = queryHotWeggler(req, db_connection); //핫 위글러 리스트 및 동영상 데이터
+            const edition = queryEdition(req, db_connection); // 기획전 상품
 
-            const [last_order_data, gongu_deal_data, gongu_deadline_data, ad_list_data, hot_weggler_data] = await Promise.all([last_order, gongu_deal, gongu_deadline, ad_list, hot_weggler])
+            const [last_order_data, gongu_deal_data, gongu_deadline_data, ad_list_data, hot_weggler_data, edition_data] = await Promise.all([last_order, gongu_deal, gongu_deadline, ad_list, hot_weggler, edition])
 
             const hot_weggler_parse = hotWegglerParse(hot_weggler_data);
+            const edition_parse = editionParse(edition_data);
 
             req.innerBody['hot_weggler'] = hot_weggler_parse;
             req.innerBody['last_order'] = last_order_data;
             req.innerBody['gongu_deal'] = gongu_deal_data;
             req.innerBody['gongu_deadline'] = gongu_deadline_data;
             req.innerBody['ad_list'] = ad_list_data;
+            req.innerBody['edition'] = edition_parse
 
             deleteBody(req);
             sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
@@ -151,6 +154,29 @@ function hotWegglerParse(hotWeggler) {
             user_profile_image: item.user_profile_image,
             user_nickname: item.user_nickname,
             video_info: item.video_info? item.video_info.split('@!@').map(info_item=> JSON.parse(info_item)) : []
+        }
+    })
+}
+
+function queryEdition(req, db_connection) {
+    const _funcName = arguments.callee.name;
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_select_searchview_gongu_edition_v1'
+        , [
+            req.headers['user_uid'],
+            // req.paramBody['product_uid'],
+        ]
+    );
+}
+
+function editionParse(edition) {
+    return edition.map(item=>{
+        return {
+            edition_uid: item.edition_uid,
+            start_time: item.start_time,
+            end_time: item.end_time,
+            edition_name: item.edition_name,
+            edition_list: item.edition_list? item.edition_list.split('@!@').map(info_item=> JSON.parse(info_item)) : []
         }
     })
 }
