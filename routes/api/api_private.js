bd1db1fb6c46618f14ba644f9673d863992bef7e
jonghao2 <weggle_khaki@weggle.io>
2022-09-14 14:26:13 +0900

    팔로우, 팔로워 검색 기능 구현

--- routes/api/api_private.js
+++ routes/api/api_private.js
@@ -177,6 +177,8 @@ app.route('/follow/list').get( require('./follow/selectFollowList') )
 app.route('/follow/find/list').get( require('./follow/selectFollowFindList') )
 app.route('/v1/follow/list').get( require('./follow/v1SelectFollowList') )
 app.route('/v1/follower/list').get( require('./follow/v1SelectFollowerList') )
+app.route('/v1/follow/search/list').get( require('./follow/v1SelectFollowSearchList') )
+app.route('/v1/follower/search/list').get( require('./follow/v1SelectFollowerSearchList') )
 
 /**
  * searchView api
