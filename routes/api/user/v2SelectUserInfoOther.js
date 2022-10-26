b9b208c315d5bbaf0420193b524b62738c72f243
jonghao2 <weggle_khaki@weggle.io>
2022-10-26 16:21:12 +0900

    팔로우 체크 다시 변경

--- routes/api/user/v2SelectUserInfoOther.js
+++ routes/api/user/v2SelectUserInfoOther.js
@@ -56,7 +56,8 @@
  
          mysqlUtil.connectPool(async function (db_connection) {
              req.innerBody = {};
- 
+             console.log(req.paramBody.user_uid, '============user_uid')
+             console.log(req.headers.user_uid, '============token user_uid')
              req.innerBody['item'] = await querySelect(req, db_connection);
              if (!req.innerBody['item']) {
                  errUtil.createCall(errCode.empty, `회원가입하지 않은 유저입니다.`)
