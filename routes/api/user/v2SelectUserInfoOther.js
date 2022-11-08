10ab21c8a2a5227a33d0ae45f7220dd932ccb685
jonghao2 <weggle_khaki@weggle.io>
2022-11-08 12:23:15 +0900

    /v2/user/info/other video count + photo count 로직 추가

--- routes/api/user/v2SelectUserInfoOther.js
+++ routes/api/user/v2SelectUserInfoOther.js
@@ -32,97 +32,99 @@
  *           $ref: '#/definitions/Error'
  */
 
- const paramUtil = require('../../../common/utils/paramUtil');
- const fileUtil = require('../../../common/utils/fileUtil');
- const mysqlUtil = require('../../../common/utils/mysqlUtil');
- const sendUtil = require('../../../common/utils/sendUtil');
- const errUtil = require('../../../common/utils/errUtil');
- const logUtil = require('../../../common/utils/logUtil');
- 
- const errCode = require('../../../common/define/errCode');
- 
- let file_name = fileUtil.name(__filename);
- 
- module.exports = function (req, res) {
-     const _funcName = arguments.callee.name;
- 
-     try {
-         req.file_name = file_name;
-         logUtil.printUrlLog(req, `== function start ==================================`);
-         req.paramBody = paramUtil.parse(req);
-         // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
- 
-         checkParam(req);
- 
-         mysqlUtil.connectPool(async function (db_connection) {
-             req.innerBody = {};
-             console.log(req.paramBody.user_uid, '============user_uid')
-             console.log(req.headers.user_uid, '============token user_uid')
-             req.innerBody['item'] = await querySelect(req, db_connection);
-             if (!req.innerBody['item']) {
-                 errUtil.createCall(errCode.empty, `회원가입하지 않은 유저입니다.`)
-                 return
-             }
- 
-             let follow_data = await queryFollowCheck(req, db_connection);
-             req.innerBody['item']['is_follow'] = follow_data? 1 : 0
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
 
-             const interests = await querySelectInterest(req, db_connection);
-             const allInterests = await querySelectAllInterest(req, db_connection)
-             console.log(interests, '======================>>>>>>>>>>interests')
-             req.innerBody['item']['my_interests'] = [...interests];
-             req.innerBody['item']['all_interests'] = [...allInterests]
- 
-             deleteBody(req)
-             sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
- 
-         }, function (err) {
-             sendUtil.sendErrorPacket(req, res, err);
-         });
- 
-     } catch (e) {
-         let _err = errUtil.get(e);
-         sendUtil.sendErrorPacket(req, res, _err);
-     }
- }
- 
- function checkParam(req) {
-     paramUtil.checkParam_noReturn(req.paramBody, 'user_uid');
- }
- 
- function deleteBody(req) {
-     // delete req.innerBody['item']['latitude']
-     // delete req.innerBody['item']['longitude']
-     delete req.innerBody['item']['push_token']
-     delete req.innerBody['item']['access_token']
- }
- 
- function querySelect(req, db_connection) {
-     const _funcName = arguments.callee.name;
- 
-     return mysqlUtil.querySingle(db_connection
-         , 'call proc_select_user_info'
-         , [
-             req.paramBody['user_uid'],
-             // req.headers['access_token'],
-         ]
-     );
- }
- 
- function queryFollowCheck(req, db_connection) {
-     const _funcName = arguments.callee.name;
- 
-     return mysqlUtil.querySingle(db_connection
-         , 'call proc_select_follow_check'
-         , [
-             req.headers['user_uid'],
-             req.paramBody['user_uid'],
- 
-         ]
-     );
- }
+const errCode = require('../../../common/define/errCode');
 
- function querySelectAllInterest(req, db_connection) {
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+        // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+            console.log(req.paramBody.user_uid, '============user_uid')
+            console.log(req.headers.user_uid, '============token user_uid')
+            req.innerBody['item'] = await querySelect(req, db_connection);
+            if (!req.innerBody['item']) {
+                errUtil.createCall(errCode.empty, `회원가입하지 않은 유저입니다.`)
+                return
+            }
+
+            req.innerBody.item['video_count'] = req.innerBody.item['video_count'] + req.innerBody.item['photo_count']
+
+            let follow_data = await queryFollowCheck(req, db_connection);
+            req.innerBody['item']['is_follow'] = follow_data? 1 : 0
+            
+            const interests = await querySelectInterest(req, db_connection);
+            const allInterests = await querySelectAllInterest(req, db_connection)
+            console.log(interests, '======================>>>>>>>>>>interests')
+            req.innerBody['item']['my_interests'] = [...interests];
+            req.innerBody['item']['all_interests'] = [...allInterests]
+
+            deleteBody(req)
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+    paramUtil.checkParam_noReturn(req.paramBody, 'user_uid');
+}
+
+function deleteBody(req) {
+    // delete req.innerBody['item']['latitude']
+    // delete req.innerBody['item']['longitude']
+    delete req.innerBody['item']['push_token']
+    delete req.innerBody['item']['access_token']
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_user_info'
+        , [
+            req.paramBody['user_uid'],
+            // req.headers['access_token'],
+        ]
+    );
+}
+
+function queryFollowCheck(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_follow_check'
+        , [
+            req.headers['user_uid'],
+            req.paramBody['user_uid'],
+
+        ]
+    );
+}
+
+function querySelectAllInterest(req, db_connection) {
     const _funcName = arguments.callee.name;
     return mysqlUtil.queryArray(db_connection
         , 'call proc_select_interest_keyword_v2'
