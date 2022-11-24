7a995f3f1f76268ca9e2f302a4f3bd25c23617fa
jonghao2 <weggle_khaki@weggle.io>
2022-11-24 17:47:40 +0900

    공구해요 상세페이지 불러오는 api 생성

---
+++ routes/api/weggler/v2SelectBuyTogetherDetail.js
@@ -0,0 +1,73 @@
+/**
+ * Created by jongho
+ *
+ * @swagger
+ * /api/private/v2/weggler/buytogether/detail:
+ *   get:
+ *     summary: 공구해요 상세페이지
+ *     tags: [Weggler]
+ *     description: |
+ *      ## path : /api/private/v2/weggler/buytogether/detail
+ *
+ *       * 공구해요 게시물 상세페이지
+ * 
+ *     parameters:
+ *       - in: query
+ *         name: post_uid
+ *         required: true
+ *         schema:
+ *           type: number
+ *           example: 77
+ *         description: |
+ *           offset
+ *
+ *     responses:
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+const dateUtil = require('../../../common/utils/dateUtil')
+
+
+let file_name = fileUtil.name(__filename);
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+        req.innerBody = {};
+
+        req.innerBody['item'] = await queryFollowFeedList(req, db_connection);
+        
+        sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+async function queryFollowFeedList(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_weggler_buytogether_detail_v2'
+        , [
+            req.headers['user_uid'],
+            req.paramBody['post_uid'],
+        ]
+    );
+}
