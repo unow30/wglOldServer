c3427e153b0cef2164fbb22579866f91a88d4abc
jonghao2 <weggle_khaki@weggle.io>
2022-12-06 14:04:56 +0900

    스토리 리스트 http 메소드 추가

--- routes/api/api_private.js
+++ routes/api/api_private.js
@@ -277,7 +277,7 @@ app.route('/v2/weggler/recommend/review/list').get( require('./weggler/v2SelectR
 app.route('/v2/weggler/community/post').delete( require('./weggler/v2DeleteCommunityPost') )
 app.route('/v2/weggler/community/post').put( require('./weggler/v2UpdateCommunityPost') )
 app.route('/v2/weggler/story/visit').post( require('./weggler/v2CreateWegglerStoryVisit') )
-app.route('/v2/weggler/follow/feed/story/list').post( require('./weggler/v2SelectFeedStoryList') )
+app.route('/v2/weggler/follow/feed/story/list').get( require('./weggler/v2SelectFeedStoryList') )
 app.route('/v2/weggler/story/detail').get( require('./weggler/v2SelectWegglerStoryDetail') )
 app.route('/v2/weggler/story/detail/simple').get( require('./weggler/v2SelectWegglerStoryDetailSimple') )
 app.route('/v2/weggler/ranking').get( require('./weggler/v2SelectRankingWeggler')) // 위글러 랭킹 위글러
