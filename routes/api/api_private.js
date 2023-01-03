d0680ccfcb3c478612399c47a71f8c4ae5195046
jonghao2 <weggle_khaki@weggle.io>
2023-01-03 17:41:49 +0900

    리뷰 생성 전 챌린지 유무 확인 api 작성

--- routes/api/api_private.js
+++ routes/api/api_private.js
@@ -352,5 +352,6 @@ app.route('/v2/event/order').post( require('./event/v2CreateEventOrder'))//이�
  */
 app.route('/v2/challenge/list').get( require('./challenge/v2SelectChallengeList')) // 챌린지 리스트
 app.route('/v2/challenge/detail/list').get( require('./challenge/v2SelectChallengeDetailList')) // 챌린지 디테일 리스트
+app.route('/v2/challenge/product/check').get( require('./challenge/v2SelectChallengeProductCheck')) // 챌린지 리뷰 생성 전 진행중인 챌린지 유무 확인 및 상품 정보 받아옴
 
 module.exports = app;
