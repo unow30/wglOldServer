384e219bd43419e6a40aba4ff5ed3ccfc8beb1a7
unow30 <https://github.com/unow30>
2023-04-04 16:03:44 +0900

    feat: 피드리스트 v3 추가, 인기순 영상 필터 지정3
    피드리스트 v3 추가, 인기순 영상 필터 지정3
    proc_select_feed_recommend_list_v3 카테고리, 필터링 표시 제거

--- apiDocs/v3/feed/v3SelectFeedListDoc.js
+++ apiDocs/v3/feed/v3SelectFeedListDoc.js
@@ -23,10 +23,10 @@
  *           example: all
  *         description: |
  *           all or null: 피드리스트 실행
- *           favorite: 위글 추천 영상
+ *           favorite: 위글 추천 영상(카테고리, 필터링 선택 없음)
  *           brand: 브랜드관
  *           groupbuying: 공동구매(이전꺼실행, 필터링 및 기타이슈 수정중)
- *           challenge: 첼린지 영상
+ *           challenge: 첼린지 영상(카테고리, 필터링 선택 없음)
  *         enum: ['', all, favorite, brand, groupbuying, challenge]
  *       - in: query
  *         name: latitude
