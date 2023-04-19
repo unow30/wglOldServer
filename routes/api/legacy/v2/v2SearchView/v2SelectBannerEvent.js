045b506c34a62fecad3b65f0b2aa9ac43462a06b
wegglecharcoal <84115242+wegglecharcoal@users.noreply.github.com>
2023-04-19 16:26:59 +0900

    Feat/bootpay verify ios (#318)
    * fix: 배너 이벤트 리스트 api에 edition_uid 추가
    
    * fix: 배너 이벤트 리스트 api에 edition_uid 추가
    
    ---------
    
    Co-authored-by: unow30 <https://github.com/unow30>

--- routes/api/legacy/v2/v2SearchView/v2SelectBannerEvent.js
+++ routes/api/legacy/v2/v2SearchView/v2SelectBannerEvent.js
@@ -10,9 +10,11 @@
  *      ## path : /api/public/v2/searchview/banner/event
  *
  *         * ## 배너클릭시 이벤트페이지로 이동하기
- *         * ### 230117기준 위글 밀키트 모음전 뷰를 위한 데이터를 보내준다.
- *         * 두줄씩 26개 상품 표시, 인기순(랜덤)으로만 표시, 다른 필터링이나 기능 없다.
- *         * 이 api를 재활용할수 없을까?
+ *         * ## 230117기준 위글 밀키트 모음전 뷰를 위한 데이터를 보내준다.
+ *         * ## 두줄씩 26개 상품 표시, 인기순(랜덤)으로만 표시, 다른 필터링이나 기능 없다.
+ *         * ## 기획전 edition_uid를 받아서 기획전 상품정보를 전달한다.
+ *         * ## 기획전 이벤트가 진행중인 경우에만 데이터를 볼 수 있다.
+ *
  *
  *     parameters:
  *       - in: query
@@ -32,6 +34,15 @@
  *           example: 133q1234
  *         description: |
  *           검색할 때 필요한 랜덤 시드입니다.
+ *       - in: query
+ *         name: edition_uid
+ *         default: 0
+ *         required: true
+ *         schema:
+ *           type: number
+ *           example: 0
+ *         description: |
+ *           기획전 uid
  *
  *     responses:
  *       400:
@@ -83,6 +94,7 @@ function queryEventMealkitList(req, db_connection, date){
             req.headers['user_uid'],
             date,
             req.paramBody['random_seed'],
+            req.paramBody['edition_uid'],
             req.paramBody['offset'],
         ]
     );
