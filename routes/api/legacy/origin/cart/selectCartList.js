94881d42f99924f7a41ad3fc2548eb31aae48575
charcoal <weggle_charcoal@weggle.io>
2023-11-20 14:43:41 +0900

    feat: selectCartList.js update
    장바구니 리스트 일반구매, 인플루언서 공동구매 구분

--- routes/api/legacy/origin/cart/selectCartList.js
+++ routes/api/legacy/origin/cart/selectCartList.js
@@ -22,82 +22,85 @@
  *           $ref: '#/definitions/Error'
  */
 
-const paramUtil = require('../../../../../common/utils/legacy/origin/paramUtil');
-const fileUtil = require('../../../../../common/utils/legacy/origin/fileUtil');
-const mysqlUtil = require('../../../../../common/utils/legacy/origin/mysqlUtil');
-const sendUtil = require('../../../../../common/utils/legacy/origin/sendUtil');
-const errUtil = require('../../../../../common/utils/legacy/origin/errUtil');
-const logUtil = require('../../../../../common/utils/legacy/origin/logUtil');
+const paramUtil = require("../../../../../common/utils/legacy/origin/paramUtil");
+const fileUtil = require("../../../../../common/utils/legacy/origin/fileUtil");
+const mysqlUtil = require("../../../../../common/utils/legacy/origin/mysqlUtil");
+const sendUtil = require("../../../../../common/utils/legacy/origin/sendUtil");
+const errUtil = require("../../../../../common/utils/legacy/origin/errUtil");
+const logUtil = require("../../../../../common/utils/legacy/origin/logUtil");
 
 let file_name = fileUtil.name(__filename);
 
 module.exports = function (req, res) {
-    const _funcName = arguments.callee.name;
+  const _funcName = arguments.callee.name;
 
-    try {
-        req.file_name = file_name;
-        logUtil.printUrlLog(req, `== function start ==================================`);
-        req.paramBody = paramUtil.parse(req);
-        // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
+  try {
+    req.file_name = file_name;
+    logUtil.printUrlLog(
+      req,
+      `== function start ==================================`,
+    );
+    req.paramBody = paramUtil.parse(req);
+    // logUtil.printUrlLog(req, `param: ${JSON.stringify(req.paramBody)}`);
 
-        checkParam(req);
+    checkParam(req);
 
-        mysqlUtil.connectPool(async function (db_connection) {
-            req.innerBody = {};
+    mysqlUtil.connectPool(
+      async function (db_connection) {
+        req.innerBody = {};
 
-            // let count_data = await querySelectTotalCount(req, db_connection);
-            req.innerBody['item'] = await querySelect(req, db_connection);
-            if( req.innerBody['item'] ){
-                for( let idx in req.innerBody['item'] ){
-                    req.innerBody['item'][idx]['cart_product_list'] = JSON.parse(req.innerBody['item'][idx]['cart_product_list'])
-                }
-            }
-            // req.innerBody['total_count'] = count_data['total_count'];
+        // let count_data = await querySelectTotalCount(req, db_connection);
+        req.innerBody["item"] = await querySelect(req, db_connection);
+        if (req.innerBody["item"]) {
+          for (let idx in req.innerBody["item"]) {
+            console.log(req.innerBody["item"][idx]);
+            console.log(idx);
+            req.innerBody["item"][idx]["cart_product_list"] = JSON.parse(
+              req.innerBody["item"][idx]["cart_product_list"],
+            );
+          }
+        }
+        // req.innerBody['total_count'] = count_data['total_count'];
 
-            deleteBody(req)
-            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
-
-        }, function (err) {
-            sendUtil.sendErrorPacket(req, res, err);
-        });
-
-    } catch (e) {
-        let _err = errUtil.get(e);
-        sendUtil.sendErrorPacket(req, res, _err);
-    }
-}
+        deleteBody(req);
+        sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+      },
+      function (err) {
+        sendUtil.sendErrorPacket(req, res, err);
+      },
+    );
+  } catch (e) {
+    let _err = errUtil.get(e);
+    sendUtil.sendErrorPacket(req, res, _err);
+  }
+};
 
 function checkParam(req) {
-    // paramUtil.checkParam_noReturn(req.paramBody, 'last_uid');
+  // paramUtil.checkParam_noReturn(req.paramBody, 'last_uid');
 }
 
 function deleteBody(req) {
-    // delete req.innerBody['item']['latitude']
-    // delete req.innerBody['item']['longitude']
-    // delete req.innerBody['item']['push_token']
-    // delete req.innerBody['item']['access_token']
+  // delete req.innerBody['item']['latitude']
+  // delete req.innerBody['item']['longitude']
+  // delete req.innerBody['item']['push_token']
+  // delete req.innerBody['item']['access_token']
 }
 
 function querySelect(req, db_connection) {
-    const _funcName = arguments.callee.name;
+  const _funcName = arguments.callee.name;
 
-    return mysqlUtil.queryArray(db_connection
-        , 'call proc_select_cart_list'
-        , [
-            req.headers['user_uid'],
-            // req.paramBody['last_uid'],
-        ]
-    );
+  return mysqlUtil.queryArray(db_connection, "call proc_select_cart_list", [
+    req.headers["user_uid"],
+    // req.paramBody['last_uid'],
+  ]);
 }
 
 function querySelectTotalCount(req, db_connection) {
-    const _funcName = arguments.callee.name;
+  const _funcName = arguments.callee.name;
 
-    return mysqlUtil.querySingle(db_connection
-        , 'call proc_select_cart_total_count'
-        , [
-            req.headers['user_uid'],
-        ]
-    );
+  return mysqlUtil.querySingle(
+    db_connection,
+    "call proc_select_cart_total_count",
+    [req.headers["user_uid"]],
+  );
 }
-
